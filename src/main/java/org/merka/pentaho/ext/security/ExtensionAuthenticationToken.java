d1d82226c884a647f1c8c3615a8404de72964e3a
unknown <alberto.mercati@gmail.com>
2015-02-22 13:03:47 +0100

    added license notices to source files

--- src/main/java/org/merka/pentaho/ext/security/ExtensionAuthenticationToken.java
+++ src/main/java/org/merka/pentaho/ext/security/ExtensionAuthenticationToken.java
@@ -1,9 +1,29 @@
+/*
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.merka.pentaho.ext.security;
 
 import org.springframework.security.GrantedAuthority;
 import org.springframework.security.providers.AbstractAuthenticationToken;
 
 public class ExtensionAuthenticationToken extends AbstractAuthenticationToken{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8992885651997621514L;
 	
 	private Object principal;
 	private Object credentials;
