110027b7d10a0e6a765c7d2f0d93e57977ddfc98
alberto.mercati@gmail.com <alberto.mercati@gmail.com>
2015-08-30 16:10:37 +0200

    changed pentaho dependencies version to 5.4.0.1-130; added jackson with test scope

--- src/test/java/org/merka/pentaho/ext/web/LoginTicketGeneratorFilterTest.java
+++ src/test/java/org/merka/pentaho/ext/web/LoginTicketGeneratorFilterTest.java
@@ -8,12 +8,17 @@ import javax.servlet.ServletException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.netbeans.mdr.persistence.btreeimpl.btreestorage.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockFilterChain;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.TextNode;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:/web-test-context.xml"})
@@ -37,6 +42,15 @@ public class LoginTicketGeneratorFilterTest {
 		String content = response.getContentAsString();
 		assertNotNull(content);
 		
+		ObjectMapper mapper = new ObjectMapper();
+		JsonNode node = mapper.readTree(content);
+		assertNotNull(node.get("ticketId"));
+		JsonNode valueNode = node.findPath("ticketId");
+		assertNotNull(valueNode);
+		assertTrue(valueNode instanceof TextNode);
+		TextNode textNode = (TextNode)valueNode;
+		UUID uuid = new UUID(textNode.textValue());
+		assertNotNull(uuid);
 	}
 
 }
